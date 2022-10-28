b397e91aaccf1c83f6e7ef2429dade4fde691376
denis0108 <71262767+denis0108@users.noreply.github.com>
2022-10-28 15:10:39 +0300

    Controlling and Uncontrolling component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 // import Counter from "./components/Counter";
 // import ClassCounter from "./components/ClassCounter";
 import './styles/App.css'
 // import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/Input/MyInput";
 
 
 function App() {
@@ -14,12 +16,31 @@ function App() {
         {id:4, title: 'Javascript 4', body:'Description'}
     ])
 
+    const [title, setTitle] = useState('')
+    const bodyInputRef = useRef()
+
+    function addNewPost(event) {
+        event.preventDefault()
+        console.log(title)
+        console.log(bodyInputRef.current.value)
+    }
+
     return (
     <div className="App">
         <form>
-            <input type="text" placeholder={"Название поста"}/>
-            <input type="text" placeholder={"Описание поста"}/>
-            <button>Создать пост</button>
+            {/*Управляемый компонент*/}
+            <MyInput
+                value={title}
+                type="text"
+                onChange={(e)=>setTitle(e.target.value)}
+                placeholder={"Название поста"}
+            />
+            {/*Неуправляемый компонент*/}
+            <MyInput
+                ref={bodyInputRef}
+                type="text"
+                placeholder={"Описание поста"}/>
+            <MyButton onClick={addNewPost}>Создать пост</MyButton>
         </form>
         <PostList posts={posts} title={"Javascript"}/>
     </div>
