d8861c7e9e65ed37e7ef1de9cd53bfa66ebcd258
denis0108 <71262767+denis0108@users.noreply.github.com>
2022-10-28 09:17:04 +0300

    *.modules.css nice fitch!

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 // import Counter from "./components/Counter";
 // import ClassCounter from "./components/ClassCounter";
 import './styles/App.css'
-import PostItem from "./components/PostItem";
+// import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 
 
@@ -14,21 +14,24 @@ function App() {
         {id:4, title: 'Javascript 4', body:'Description'}
     ])
 
-    const [posts2, setPosts2] = useState([
-        {id:1, title: 'Python', body:'Description'},
-        {id:2, title: 'Python 2', body:'Description'},
-        {id:3, title: 'Python 3', body:'Description'},
-        {id:4, title: 'Python 4', body:'Description'}
-    ])
-
     return (
     <div className="App">
-        {/*<h1 className={"title"}>Список постов</h1>*/}
+        <form>
+            <input type="text" placeholder={"Название поста"}/>
+            <input type="text" placeholder={"Описание поста"}/>
+            <button>Создать пост</button>
+        </form>
         <PostList posts={posts} title={"Javascript"}/>
-        <PostList posts={posts2} title={"Python"}/>
-
     </div>
   );
 }
 
 export default App;
+
+// <PostList posts={posts2} title={"Python"}/>
+// const [posts2, setPosts2] = useState([
+//     {id:1, title: 'Python', body:'Description'},
+//     {id:2, title: 'Python 2', body:'Description'},
+//     {id:3, title: 'Python 3', body:'Description'},
+//     {id:4, title: 'Python 4', body:'Description'}
+// ])
