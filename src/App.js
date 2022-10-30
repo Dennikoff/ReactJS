143a4a5094887f76014afe12e5df8b72c69842b9
denis0108 <71262767+denis0108@users.noreply.github.com>
2022-10-30 17:39:25 +0300

    make some project-files layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useMemo} from "react";
+import React, {useState, useMemo, useEffect} from "react";
 // import Counter from "./components/Counter";
 // import ClassCounter from "./components/ClassCounter";
 import './styles/App.css'
@@ -15,6 +15,8 @@ import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
 import {queries} from "@testing-library/react";
 import axios from "axios";
+import PostService from "./API/PostService";
+import {wait} from "@testing-library/user-event/dist/utils";
 
 
 function App() {
@@ -24,14 +26,19 @@ function App() {
     const [modalVisible, setModalVisible] = useState(false)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
+    //one time
+    useEffect(() => {
+        fetchPosts()
+    }, [])
+
     function createPost(newPost) {
         setPosts([...posts, newPost]);
         setModalVisible(false)
     }
 
     async function fetchPosts() {
-        const resonse = await axios.get('https://jsonplaceholder.typicode.com/posts')
-        setPosts(resonse.data)
+        const posts = await PostService.getAll()
+        setPosts(posts)
     }
 
     function removePost(post) {
@@ -40,8 +47,6 @@ function App() {
 
     return (
         <div className="App">
-            <MyButton onClick={fetchPosts}>Get posts</MyButton>
-            <br/>
             <MyButton style={{marginTop: '15px'}}
                 onClick={() => setModalVisible(true)}>
                 Создать пост
