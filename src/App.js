6fbb3185f5ce209128eb58701ddb6fc21061fa4c
denis0108 <71262767+denis0108@users.noreply.github.com>
2022-10-30 18:17:28 +0300

    add new hook

--- src/App.js
+++ src/App.js
@@ -18,18 +18,22 @@ import {usePosts} from "./hooks/usePosts";
 import PostService from "./API/PostService";
 // import {wait} from "@testing-library/user-event/dist/utils";
 import Loader from './components/UI/Loader/Loader'
+import {useFetching} from "./hooks/useFetching";
 
 
 function App() {
-    const [isPostsLoading, setIsPostsLoading] = useState(false)
     const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modalVisible, setModalVisible] = useState(false)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+        const posts = await PostService.getAll()
+        setPosts(posts)
+    })
 
     //one time
     useEffect(() => {
-        fetchPosts()
+        fetchPosts();
     }, [])
 
     function createPost(newPost) {
@@ -37,14 +41,8 @@ function App() {
         setModalVisible(false)
     }
 
-    async function fetchPosts() {
-        setIsPostsLoading(true)
-        setTimeout(async () => {
-            const posts = await PostService.getAll()
-            setIsPostsLoading(false)
-            setPosts(posts)
-            }, 1000)
-    }
+
+
 
     function removePost(post) {
         setPosts(posts.filter(p => p.id !== post.id))
@@ -64,6 +62,9 @@ function App() {
             <PostFilter
                 filter={filter}
                 setFilter={setFilter}/>
+            {postError &&
+                <h1>Произошла ошибка ${postError}</h1>
+            }
             {isPostsLoading
                 ? <div style={{
                     display: 'flex',
